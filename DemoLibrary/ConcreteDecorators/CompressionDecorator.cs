e489657f9ca61fae2855f5b1636d7e8fdde17482
Parsa <mehdipourparsa@gmail.com>
2024-08-29 00:30:03 -0400

    feat : add concrete decorators

---
+++ DemoLibrary/ConcreteDecorators/CompressionDecorator.cs
@@ -0,0 +1,26 @@
+﻿using DemoLibrary.BaseDecorators;
+using DemoLibrary.ComponentInterfaces;
+
+namespace DemoLibrary.ConcreteDecorators;
+
+/// <summary>
+/// Concrete Decorators call the wrapped object and alter its result in some way.
+/// </summary>
+public class CompressionDecorator : DataSourceDecorator
+{
+    public CompressionDecorator(DataSource source) : base(source) { }
+
+    public override void WriteData(string data)
+    {
+        Console.WriteLine("Compressing data");
+        base.WriteData($"Compressed({data})");
+    }
+
+    public override string ReadData()
+    {
+        string data = base.ReadData();
+        Console.WriteLine("Decompressing data");
+        Console.WriteLine($"Data is now : {data.Replace("Compressed(", "").Replace(")", "")}");
+        return data.Replace("Compressed(", "").Replace(")", "");
+    }
+}
---
+++ DemoLibrary/ConcreteDecorators/EncryptionDecorator.cs
@@ -0,0 +1,26 @@
+﻿using DemoLibrary.BaseDecorators;
+using DemoLibrary.ComponentInterfaces;
+
+namespace DemoLibrary.ConcreteDecorators;
+
+/// <summary>
+/// Concrete Decorators call the wrapped object and alter its result in some way.
+/// </summary>
+public class EncryptionDecorator : DataSourceDecorator
+{
+    public EncryptionDecorator(DataSource source) : base(source) { }
+
+    public override void WriteData(string data)
+    {
+        Console.WriteLine("Encrypting data");
+        base.WriteData($"Encrypted({data})");
+    }
+
+    public override string ReadData()
+    {
+        string data = base.ReadData();
+        Console.WriteLine("Decrypting data");
+        Console.WriteLine($"Data is now : {data.Replace("Encrypted(", "").Replace(")", "")}");
+        return data.Replace("Encrypted(", "").Replace(")", "");
+    }
+}
