29cc7bb680336cbca09b0e5e9457e6b8b4c4e3ad
Parsa <mehdipourparsa@gmail.com>
2024-08-29 00:29:28 -0400

    feat : add component interfaces

---
+++ DemoLibrary/ComponentInterfaces/DataSource.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoLibrary.ComponentInterfaces;
+
+/// <summary>
+///The base Component interface/(abstract class) defines operations that can be altered by
+/// decorators.
+/// </summary>
+public abstract class DataSource
+{
+    public abstract void WriteData(string data);
+    public abstract string ReadData();
+}
